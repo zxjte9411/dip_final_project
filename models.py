6e406bbd7185183b66d3b0259a9a6030ab87e1c4
zxjte9411 <a5852241@gmail.com>
2019-06-15 15:11:46 +0800

    create camera model

---
+++ models.py
@@ -0,0 +1,29 @@
+import cv2
+import threading
+class Camera:
+    def __init__(self, cam_num):
+        self.cam_num = cam_num
+        self.cap = None
+
+    def initialize(self):
+        self.cap = cv2.VideoCapture(self.cam_num)
+
+    def get_frame(self):
+        ret, self.last_frame = self.cap.read()
+        return self.last_frame
+
+    def set_brightness(self, value):
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, value)
+
+    def get_brightness(self):
+        return self.cap.get(cv2.CAP_PROP_BRIGHTNESS)
+
+    def close_camera(self):
+        self.cap.release()
+
+    def __str__(self):
+        return f'OpenCV Camera {self.cam_num}'
+
+    def __del__(self):
+        self.close_camera()
+        # print(f'camera {self.cam_num} is release!')
