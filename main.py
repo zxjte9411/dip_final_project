c400b2fe7586324bbb499b4ac8dcadf2885eb6bc
zxjte9411 <a5852241@gmail.com>
2019-06-15 15:11:59 +0800

    init main

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication
+
+from models import Camera
+from view import StartWindow
+
+if __name__ == "__main__":
+    camera = Camera(0)
+    camera.initialize()
+
+    app = QApplication([])
+    start_window = StartWindow(camera)
+    start_window.show()
+    app.exit(app.exec_())
